6ae7688a67d73ffc0f9231c88b3e52b7ca6d3a58
varunudayshet <varunushet@gmail.com>
2025-02-09 23:11:11 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,23 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.querySelector("form");
 
   loginForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent form submission
+    e.preventDefault(); 
 
-    // Get user input from the form using IDs
     const username = document.querySelector("#username").value;
     const password = document.querySelector("#password").value;
-
-    // Get stored user details from localStorage
     const storedUser = JSON.parse(localStorage.getItem("user"));
-
-    // Validate the login
     if (
       storedUser &&
       storedUser.username === username &&
       storedUser.password === password
     ) {
       alert("Login Successful!");
-      window.location.href = "index.html"; // Redirect to the dashboard page
+      window.location.href = "index.html"; 
     } else {
       alert("Invalid credentials. Please try again.");
     }
